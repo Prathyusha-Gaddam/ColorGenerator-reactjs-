8e934283626aa8e363c86bf97de01e3bb08379ac
Prathyusha-Gaddam <prathyushagaddam76@gmail.com>
2023-05-09 22:52:39 +0530

    first

---
+++ Springbootdb/src/main/java/org/studyeasy/SpringbootdbApplication.java
@@ -0,0 +1,13 @@
+package org.studyeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdbApplication.class, args);
+	}
+
+}
